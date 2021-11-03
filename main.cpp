a6e1da67a669c3772a7280f1172d18b272604e14
Szymon Motyka <gogi25a@o2.pl>
2021-11-03 13:53:40 +0100

    Miejsca zerowe ver(1.2.2)

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
 int main ()
 {
-	int a,b;
-	cout << " Podaj wartosc a: " << endl;
+	float a,b,X0;
+	
+	cout << "Program do obliczania miejsca zerowego funkcji liniowej." << endl << endl;
+	
+	cout << "Wpisz wartosc wspolczynnika kierunkowego a: " << endl;
 	cin >> a;
+	cout << "Wpisz wartosc wyrazu wolnego funkcji: " << endl;
+	cin >> b;
+	
 	if (a==0)
 	{
-		cout << "Miejsce zerowe nie wystepuje." << endl;
-		return 0;
+	 cout << "Nie istnieje miejsce zerowe funkcji." << endl;
+	 return 0;
 	}
-	cout << "Podaj wartosc b: " << endl;
-	cin >> b;
-	cout << "Miejsce zerowe funkcji liniowej wynosi: " << (-b)/a << endl;
+	
+	if (b==0)
+	{ 
+	 cout << "Funkcja ma nieskonczenie wiele miejsc zerowych." << endl;
+	 return 0;
+	}
+	   
+	X0 = ((-b)/a);
+	cout << "Miejsce zerowe funkcji wynosi: " <<X0<< endl;  
+	
 	return 0;
 }
