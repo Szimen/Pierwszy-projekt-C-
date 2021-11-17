5af55c6b0ac7431b6b6150f5995ca28420be17e3
Szimen <gogi25a@o2.pl>
2021-11-17 10:01:21 +0100

    Miejsca zerowe ver.(2.4)

--- main.cpp
+++ main.cpp
@@ -2,11 +2,67 @@
 #include <math.h>
 
 using namespace std;
+
+void zamien_na_kanoniczne(float a, float p, float q, float& b, float& c)
+{
+    b = (-a*p*2);
+    c = (a*p*p)+q;
+
+}
+
+void oblicz_ogolna(float a, float b, float c)
+{
+    float x1,x2,X0l,X0k,delta;
+    if (a==0 & b==0)
+	{
+	   cout << "Brak miejsc zerowych." << endl;
+	   return;
+	}
+
+	if (a==0)
+	{
+	   X0l = ((-c)/b);
+	   cout << "To nie jest funkcja kwadratowa, tylko liniowa." << endl;
+	   cout << "Miejsce zerowe funkcji wynosi: " << X0l << endl;
+	   return;
+	}
+
+	delta = b*b-(4*a*c);
+
+	if (delta>=0)
+	{
+        if (delta==0)
+        {
+            X0k = (-b/(2*a));
+            cout << "Miejsce zerowe X0 wynosi: " << X0k << endl;
+            return;
+        }
+	   x1 = ((-b)+(sqrt(delta)))/(2*a);
+ 	   x2 = ((-b)-(sqrt(delta)))/(2*a);
+	   cout << "Miejsca zerowe wynosza odpowiednio: x1 = " << x1 << " , x2 = " << x2 << endl;
+
+
+	  return;
+	}
+	if (delta<0)
+	    {
+	    	cout << "Brak miejsc zerowych." << endl;
+	    	return;
+		}
+}
+
 int main ()
 {
-	float a,b,c,delta;
-	float x1,x2,X0l,X0k;
-	
+    int wybor;
+    cout<<"Wybor postaci funkcji kwadratowej!!"<<endl;
+    cout<<"1. Postac ogolna "<<endl;
+    cout<<"2. Postac kanoniczna"<<endl;
+    cin>>wybor;
+
+    if(wybor == 1)
+    {
+	float a,b,c;
+
 	cout << "Podaj wspolczynniki funkcji kwadratowej: " << endl;
 	cout << "a = ";
 	cin >> a;
@@ -15,42 +71,28 @@ int main ()
 	cout << "c = ";
 	cin >> c;
 	cout <<endl;
-	
-	if (a==0 & b==0)
-	{
-	   cout << "Brak miejsc zerowych." << endl;
-	   return 0;
-	}
-	
-	if (a==0)
-	{
-	   X0l = ((-c)/b);
-	   cout << "To nie jest funkcja kwadratowa, tylko liniowa." << endl;
-	   cout << "Miejsce zerowe funkcji wynosi: " << X0l << endl;
-	   return 0;
-	}
-	
-	delta = b*b-(4*a*c);
-	
-	if (delta>=0)
-	{	
-	   x1 = ((-b)+(sqrt(delta)))/(2*a); 
- 	   x2 = ((-b)-(sqrt(delta)))/(2*a); 	
-	   cout << "Miejsca zerowe wynosza odpowiednio: x1 = " << x1 << " , x2 = " << x2 << endl;
-	   
-	  if (delta==0)
-	  {	
-	    X0k = (-b/(2*a));
-	    cout << "Miejsce zerowe X0 wynosi: " << X0k << endl;
-	    return 0;
-	  }
-	  return 0;
-	}    
-	if (delta<0)
-	    {
-	    	cout << "Brak miejsc zerowych." << endl;
-	    	return 0;
-		}
+	oblicz_ogolna(a,b,c); //funkcja liczaca miejsca zerowe
+    }
+    if(wybor == 2)
+    {
+        float a,b,c,p,q;
+			cout<<"Miejsca zerowe funkcji kwadratowej w postaci kanonicznej y=a(x-p)^2+q" << endl;
+			cout<<"Podaj a: ";
+			cin>>a;
+			cout<<"Podaj p: ";
+			cin>>p;
+			cout<<"Podaj q: ";
+			cin>>q;
+			cout << endl;
+			if(a==0)
+			{
+				cout<<"Brak!";
+				return 0;
+			}
+			zamien_na_kanoniczne(a,p,q,b,c); //funkcja zamienia z kanonicznych na ogolne wartosci
+			oblicz_ogolna(a,b,c); //funkcja liczaca miejsca zerowe
+
+    }
 	//FUNKCJA LINIOWA VER.(1.4)
 /*	cout << " Podaj wartosc Ax: " << endl;
 	cin >> a;
@@ -76,5 +118,5 @@ int main ()
 	cout << "Miejsce zerowe funkcji liniowej wynosi: " << ((-c)/b) << endl;
 	*/
 	return 0;
-	
+
 }
